177d2035f7e5c91251879d5fe33e96093da713e5
Ari J Atori <ari.j.atori@gmail.com>
2025-09-02 00:51:29 -0400

    Added LaTeX command, updated requirements.txt to add matplotlib

---
+++ commands/latex.py
@@ -0,0 +1,43 @@
+import nextcord
+from nextcord import Interaction, SlashOption
+from nextcord.ext import commands
+import matplotlib.pyplot as plt
+import numpy as np
+from io import BytesIO
+
+class LaTeX(commands.Cog, name="latex"):
+	def __init__(self, bot):
+		self.bot = bot
+		plt.rcParams['mathtext.fontset'] = 'cm'
+
+	@nextcord.slash_command(name="latex", description="Write a LaTeX expression to render")
+	async def latex(self, interaction: Interaction, latex: str = SlashOption(description="LaTeX expression", required=True)):
+		if not latex.startswith("$"):
+			latex = "$" + latex
+		if not latex.endswith("$"):
+			latex += "$"
+
+		fig, axes = plt.subplots()
+		fig.patch.set_facecolor('black')
+		axes.set_facecolor('black')
+		fig.patch.set_visible(False)
+		axes.axis('off')
+		axes.add_patch(plt.Rectangle((0, 0), 1, 1, transform=axes.transAxes, color='black', zorder=-1))
+		axes.text(0.5, 0.5, latex, fontsize=32, ha='center', va='center', color='white')
+
+		buffer = BytesIO()
+		plt.savefig(buffer, format='png', bbox_inches='tight', pad_inches=0.2, facecolor='black', transparent=False)
+		plt.close(fig)
+		buffer.seek(0)
+
+		file = nextcord.File(fp=buffer, filename="latex.png")
+		embed = nextcord.Embed(color=0x00cdff)
+		embed.set_image("attachment://latex.png")
+		await interaction.response.send_message(embed = embed, file=file)
+
+
+	
+		
+
+def setup(bot):
+	bot.add_cog(LaTeX(bot))
